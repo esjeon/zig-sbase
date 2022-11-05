21c997bd62fb989a31500ae905a4eadf559f6581
Eon S. Jeon <esjeon@hyunmu.am>
2022-11-05 13:14:05 +0900

    reimplment ArgReader for compound flags

--- src/argreader.zig
+++ src/argreader.zig
@@ -1,23 +1,33 @@
 const std = @import("std");
 const startsWith = std.mem.startsWith;
 
-fn GenericArgReader(comptime T: type, comptime sentinel: ?u8) type {
+pub fn GenericArgReader(comptime T: type, comptime sentinel: ?u8) type {
     return struct {
         const This = @This();
 
         argv: T,
+        argc: usize,
         i: usize,
-        positional: bool,
 
-        pub fn init(argv: T) This {
+        word: []const u8,
+        j: ?usize,
+
+        finished: bool,
+
+        pub fn init(args: T) This {
             return This{
-                .argv = argv,
+                .argv = args,
+                .argc = args.len,
                 .i = 0,
-                .positional = false,
+
+                .word = undefined,
+                .j = null,
+
+                .finished = false,
             };
         }
 
-        fn get_current_word(self: *This) []const u8 {
+        fn readArg(self: *This) []const u8 {
             if (sentinel) |s| {
                 return std.mem.sliceTo(self.argv[self.i], s);
             } else {
@@ -25,60 +35,115 @@ fn GenericArgReader(comptime T: type, comptime sentinel: ?u8) type {
             }
         }
 
-        pub fn nextFlag(self: *This) ?[]const u8 {
-            if (self.positional or self.i >= self.argv.len) {
-                return null;
+        pub fn nextFlag(self: *This) ?u8 {
+            if (self.finished) return null;
+
+            if (self.j) |j| {
+                if (j < self.word.len) {
+                    const c = self.word[j];
+                    self.j = j + 1;
+                    return c;
+                } else {
+                    self.j = null;
+                }
             }
 
-            const word = self.get_current_word();
-            if (word[0] != '-') {
-                // Got a positional argument. Finish parsing flags.
-                self.positional = true;
-                return null;
-            }
+            if (self.i < self.argc) {
+                self.word = self.readArg();
+                if (self.word.len <= 1 or self.word[0] != '-') {
+                    self.finished = true;
+                    return null;
+                }
 
-            self.i += 1;
-            return word;
+                self.i += 1;
+                if (self.word.len == 2 and self.word[1] == '-') {
+                    self.finished = true;
+                    return null;
+                }
+
+                self.j = 2;
+                return self.word[1];
+            }
+            return null;
         }
 
         pub fn nextPositional(self: *This) ?[]const u8 {
-            if (!self.positional or self.i >= self.argv.len) {
-                return null;
-            }
+            if (!self.finished) return null;
 
-            const word = self.get_current_word();
-            self.i += 1;
-            return word;
+            if (self.i < self.argc) {
+                const arg = self.readArg();
+                self.i += 1;
+                return arg;
+            }
+            return null;
         }
     };
 }
 
-pub const ArgReader = GenericArgReader([][*:0]const u8, '0');
+pub const ArgReader = GenericArgReader([][:0]const u8, '\x00');
 pub const SliceArgReader = GenericArgReader([][]const u8, null);
 
-test "basic usecase" {
-    var args = [_][]const u8{ "-a", "-b", "Hello", "world" };
+test "parse flags" {
+    var args = [_][]const u8{ "-a", "-b", "-c" };
     var reader = SliceArgReader.init(args[0..]);
 
-    var ret = reader.nextFlag();
-    try std.testing.expect(ret != null);
-    try std.testing.expect(std.mem.eql(u8, ret.?, "-a"));
+    try std.testing.expect(reader.nextFlag().? == 'a');
+    try std.testing.expect(reader.nextFlag().? == 'b');
+    try std.testing.expect(reader.nextFlag().? == 'c');
+}
 
-    ret = reader.nextFlag();
-    try std.testing.expect(ret != null);
-    try std.testing.expect(std.mem.eql(u8, ret.?, "-b"));
+test "parse flags and positionals" {
+    var args = [_][]const u8{ "-a", "-b", "0", "1" };
+    var reader = SliceArgReader.init(args[0..]);
 
-    ret = reader.nextFlag();
-    try std.testing.expect(ret == null);
+    try std.testing.expect(reader.nextFlag().? == 'a');
+    try std.testing.expect(reader.nextFlag().? == 'b');
+    try std.testing.expect(reader.nextFlag() == null);
+    try std.testing.expect(std.mem.eql(u8, reader.nextPositional().?, "0"));
+    try std.testing.expect(std.mem.eql(u8, reader.nextPositional().?, "1"));
+    try std.testing.expect(reader.nextPositional() == null);
+}
 
-    ret = reader.nextPositional();
-    try std.testing.expect(ret != null);
-    try std.testing.expect(std.mem.eql(u8, ret.?, "Hello"));
+test "parse composite flags" {
+    var args = [_][]const u8{ "-abc", "-de" };
+    var reader = SliceArgReader.init(args[0..]);
 
-    ret = reader.nextPositional();
-    try std.testing.expect(ret != null);
-    try std.testing.expect(std.mem.eql(u8, ret.?, "world"));
+    try std.testing.expect(reader.nextFlag().? == 'a');
+    try std.testing.expect(reader.nextFlag().? == 'b');
+    try std.testing.expect(reader.nextFlag().? == 'c');
+    try std.testing.expect(reader.nextFlag().? == 'd');
+    try std.testing.expect(reader.nextFlag().? == 'e');
+    try std.testing.expect(reader.nextFlag() == null);
+}
 
-    ret = reader.nextPositional();
-    try std.testing.expect(ret == null);
+test "double-dash separates flags from positionals" {
+    var args = [_][]const u8{ "-a", "--", "0" };
+    var reader = SliceArgReader.init(args[0..]);
+
+    try std.testing.expect(reader.nextFlag().? == 'a');
+    try std.testing.expect(reader.nextFlag() == null);
+    try std.testing.expect(std.mem.eql(u8, reader.nextPositional().?, "0"));
+    try std.testing.expect(reader.nextPositional() == null);
+}
+
+test "single-dash is a positional" {
+    var args = [_][]const u8{ "-a", "-", "0" };
+    var reader = SliceArgReader.init(args[0..]);
+
+    try std.testing.expect(reader.nextFlag().? == 'a');
+    try std.testing.expect(reader.nextFlag() == null);
+    try std.testing.expect(std.mem.eql(u8, reader.nextPositional().?, "-"));
+    try std.testing.expect(std.mem.eql(u8, reader.nextPositional().?, "0"));
+    try std.testing.expect(reader.nextPositional() == null);
+}
+
+test "gracefully handle empty string" {
+    var args = [_][]const u8{ "-a", "", "0" };
+    var reader = SliceArgReader.init(args[0..]);
+
+    try std.testing.expect(reader.nextFlag().? == 'a');
+    try std.testing.expect(reader.nextFlag() == null);
+    try std.testing.expect(std.mem.eql(u8, reader.nextPositional().?, ""));
+    try std.testing.expect(std.mem.eql(u8, reader.nextPositional().?, "0"));
+    try std.testing.expect(reader.nextPositional() == null);
 }
